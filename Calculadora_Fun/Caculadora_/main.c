0533e57f2faf9579e283dd01ba3a7aa094b27b3f
andre <andrcosta72@gmail.com>
2022-09-14 13:54:10 -0300

    mais conteudos esteticos

--- Calculadora_Fun/Caculadora_/main.c
+++ Calculadora_Fun/Caculadora_/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <windows.h>
 
 float soma(float x, float y);
 float sub(float x, float y);
@@ -9,11 +9,19 @@ float multip(float x, float y);
 
 int main()
 {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
     
     float a,b;
     char c;
-    
-    printf("Fa%ca uma das opera%c%ces entre dois numeros ( * ,/ ,- ,+ ): ",135,135,208);
+    SetConsoleTextAttribute(hConsole, consoleInfo.wAttributes);
+    printf("\n Fa%ca uma das opera%c%ces entre",135,135,208);
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+    printf(" dois n%cmeros",163);
+    SetConsoleTextAttribute(hConsole, consoleInfo.wAttributes);
+    printf( " (*, /, -, + ): ");
+
     scanf("%f%c%f",&a,&c,&b);
 
     
@@ -40,7 +48,7 @@ int main()
         printf("Desculpe n%co entendi :/ ",198);
         break;
     }
-
+    
 }
 float soma(float x,float y){
     return(x+y);

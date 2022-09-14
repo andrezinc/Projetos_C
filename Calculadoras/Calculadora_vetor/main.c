a9267587badd689b96a6b29f462eea27f17ad063
andre <andrcosta72@gmail.com>
2022-09-14 17:33:10 -0300

    colocando um conversor de char para float

--- Calculadoras/Calculadora_vetor/main.c
+++ Calculadoras/Calculadora_vetor/main.c
@@ -1,64 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float conversor(char calculo){
-    float numeros;
-    for (int i=0; i<10; i++){
-        for(int o=0;o<10;i++){
-        if(calculo==(48+o)){
-            switch (o)
-            {
-            case 0:
-                numeros=o;
-                break;
-            case 1:
-                numeros=o;
-                break;
-            case 2:
-                numeros=o;
-                break;
-            case 3:
-                numeros=o;
-                break;
-            case 4:
-                numeros=o;
-                break;
-            case 5:
-                numeros=o;
-                break;
-            case 6:
-                numeros=o;
-                break;
-            case 7:
-                numeros=o;
-                break; 
-            case 8:
-                numeros=o;
-                break;  
-            case 9:
-                numeros=o;
-                break;                       
-            default:
-                break;
-            }
-            }
+float conversor(char calculo);
 
-        }
-    }
-    return(numeros);
-}
 int main()
 {
     char a[50];
     printf("calculo:");
-    scanf("%s",&a);
+    scanf("%s", &a);
 
     float numeros[50];
-    for(int l=0; l<10; l++){
-        numeros[l]=conversor(a[l]);
+    for (int l = 0; l < 50; l++)
+    {
+        numeros[l] = conversor(a[l]);
     }
 
-    for(int i=0; i<10;i++){
-        printf("%.1f",numeros[i]);
+    for (int i = 0; i < 50; i++)
+    {
+        printf(" %.1f ", numeros[i]);
+    }
+}
+
+float conversor(char calculo)
+{
+    float numeros;
+
+    numeros = 0;
+
+    for (int o = 48; o <= 57; o++)
+    {
+        if (calculo == (o))
+        {
+            switch (o)
+            {
+            case 48:
+                numeros = 0;
+                break;
+            case 49:
+                numeros = 1;
+                break;
+            case 50:
+                numeros = 2;
+                break;
+            case 51:
+                numeros = 3;
+                break;
+            case 52:
+                numeros = 4;
+                break;
+            case 53:
+                numeros = 5;
+                break;
+            case 54:
+                numeros = 6;
+                break;
+            case 55:
+                numeros = 7;
+                break;
+            case 56:
+                numeros = 8;
+                break;
+            case 57:
+                numeros = 9;
+                break;
+            default:
+                break;
+            }
+            return numeros;
+        }
     }
 }

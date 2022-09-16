f14dce6ea54c430bc6ad2f598de0209dce0895f7
andre <andrcosta72@gmail.com>
2022-09-16 08:57:48 -0300

    adicionando uma função de uniao dos numeros

--- Calculadoras/Calculadora_vetor/main.c
+++ Calculadoras/Calculadora_vetor/main.c
@@ -1,37 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
+int conversor(char calculo);
+int contador(char calculo);
+int uniao(int calculo, int numero);
 
-float conversor(char calculo);
-float contador(char calculo);
-int k;
-
+int k,kl;
+k=0;
+kl=0;
 int main()
 {
     char a[50];
-    int numeros[50],operac[50];
-    int l;
-    printf("calculo:");
+    int numeros[50],operac[50],testando[50];
+    int l,bn;
+    printf("calculo:(final '=')");
     scanf("%s", &a);
-
-   
     for (l = 0; l < 50; l++)
     {
         numeros[l] = conversor(a[l]); // cada numero dentro da string "a" passa pelo conversor
         operac[l]=contador(a[l]);// cada caracter é analisado dentro da função e é mostrado a posição dos caracteres;
+        bn=operac[l];
+        testando[l]=uniao(numeros[l],bn);
     }
+
     for (int i = 0; i < 50; i++)
     {
-        printf("\n numero: %i operac :%i", numeros[i], operac[i]); // aparece na tela
+      // printf("\n numero: %i operac :%i ", numeros[i], operac[i]); // aparece na tela
     }
+
 }
 
-float conversor(char calculo)
+int conversor(char calculo)
 {
     int numeros;
-
     numeros = 0;
-
     for (int o = 48; o <= 57; o++) // dentro da função se faz a comparação 10 vezes para percorrer no espectro ASCII dos caracters
     {
         if (calculo == (o)) // caso o numero dentro da string for o mesmo numero ele retorna como valor float
@@ -73,13 +76,38 @@ float conversor(char calculo)
             }
             return numeros; // retorna o numero para o vetor
         }
+        
     }
 }
-float contador(char calculo){
+int contador(char calculo){
     k++;
     if(calculo=='+'||calculo=='-'||calculo=='/'||calculo=='*'||calculo=='=')
-    {
-        
+    {     
         return k;
     }
+    else return 0;
+}
+
+int uniao(int calculo, int numero){
+    int vetor[50],a,vetor1[50],b,c,d,e;
+    b=0;
+    d=0;
+    e=0;
+    vetor[0]=0;
+    a=numero;
+    kl++;
+    vetor[kl]=calculo;
+    printf("entrada: %i",vetor[kl]);
+    printf(" a: %i",numero);
+    if(numero!=0){
+        for(c=(a-1);c>0;c--){
+            d++;
+            printf("vetordnv:%i c:%i a:%i ",vetor[d],c,(a-1));
+            e=(int)(pow(10,(c-1))+0.5);
+            printf(" e: %i ",e);
+            b=b+(vetor[d]*e);
+            printf("b: %i",b);
+        }
+    }
+    
 }
--- Função e ponteiro Medias/main.c
+++ Função e ponteiro Medias/main.c
@@ -1,41 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 float Media(float *v, int t)
-{ int i;
-float Soma;
-Soma= 0;
-for (i = 0; i < t; i++)
-Soma= Soma + v[i];
-return (Soma/t); }
-float DesvioMedio(float *v, int t) { int i;
-float *d; // verifique esta etrutura
-float Med;
-Med = Media(v, t);
-for (i = 0; i < t; i++)
-d[i] = abs(v[i] - Med);
-return(Media(d, t)); }
+{
+    int i;
+    float Soma;
+    Soma = 0;
+    for (i = 0; i < t; i++)
+        Soma = Soma + v[i];
+    return (Soma / t);
+}
+float DesvioMedio(float *v, int t)
+{
+    int i;
+    float *d; // verifique esta etrutura
+    float Med;
+    Med = Media(v, t);
+    for (i = 0; i < t; i++)
+        d[i] = abs(v[i] - Med);
+    return (Media(d, t));
+}
 
 int ArmazenaNotas(float *v)
-{ int i;
-i=0;
-printf("Digite as notas (-1 para encerrar)");
-scanf("%f",&*v);
-while (v[i]!= -1)
-{ i = i+1;
-scanf("%f", &v[i]); }
-return (i); }
-/*Função para determinar a média de uma
-relação de números*/
+{
+    int i;
+    i = 0;
+    printf("Digite as notas (-1 para encerrar)");
+    scanf("%f", &*v);
+    while (v[i] != -1)
+    {
+        i = i + 1;
+        scanf("%f", &v[i]);
+    }
+    return (i);
+}
+/*Funï¿½ï¿½o para determinar a mï¿½dia de uma
+relaï¿½ï¿½o de nï¿½meros*/
 
-int main() { int i, Quant, NotasBoas;
-float *p, Med,a;
-Quant = ArmazenaNotas(p);
-Med = Media(p, Quant);
-a=DesvioMedio(p, Quant);
-NotasBoas = 0;
-for (i = 0; i < Quant; i = i + 1)
-if (p[i] > Med)
-NotasBoas = NotasBoas + 1;
-printf("Media das notas: %f \n", Med);
-printf("Notas maiores que a media: %d",NotasBoas);
-printf("Desvio medio %f",a); }
+int main()
+{
+    int i, Quant, NotasBoas;
+    float *p, Med, a;
+    Quant = ArmazenaNotas(p);
+    Med = Media(p, Quant);
+    a = DesvioMedio(p, Quant);
+    NotasBoas = 0;
+    for (i = 0; i < Quant; i = i + 1)
+        if (p[i] > Med)
+            NotasBoas = NotasBoas + 1;
+    printf("Media das notas: %f \n", Med);
+    printf("Notas maiores que a media: %d", NotasBoas);
+    printf("Desvio medio %f", a);
+}
